7019b1e593d7397a8164d5ce3719ff4a7d661e1b
Tycho Brouwer <tychobrouwer33@gmail.com>
2023-11-03 00:35:13 +0100

    update install script to remove first

--- install.sh
+++ install.sh
@@ -2,11 +2,19 @@
 
 share=/usr/share
 
-cp color-schemes/my-theme.colors $share/color-schemes
-cp konsole/my-theme.colorscheme $share/konsole
+rm -R $share/plasma/desktoptheme/my-theme
+rm -R $share/plasma/look-and-feel/my-theme
+rm -R $share/auroae/themes/my-theme
+rm -R $share/sddm/themes/my-theme
+rm -R $share/kvantum/my-theme
+rm -R $share/wallpapers/my-theme
 
+cp -R color-schemes $share
+cp -R konsole $share
 cp -R plasma $share
 cp -R aurorae $share
 cp -R sddm $share
 cp -R wallpapers $share
 cp -R kvantum $share
+
+kquitapp5 plasmashell || killall plasmashell && kstart5 plasmashell
