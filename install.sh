01b647960b9c3d7109a6ed3314d87f6f0b630aa8
Tycho Brouwer <tychobrouwer33@gmail.com>
2023-11-02 10:34:07 +0100

    remove stuff

--- install.sh
+++ install.sh
@@ -2,10 +2,13 @@
 
 share=/usr/share
 
-cp -r Moe MoeDark $share/plasma/desktoptheme/
-cp -r Moe-Global/Moe MoeDark-Global/Moe-Dark $share/plasma/look-and-feel
-cp color-schemes/Moe.colors Moe-Dark-color-schemes/MoeDark.colors $share/color-schemes
-cp konsole/Moe.colorscheme Moe-Dark-konsole/MoeDark.colorscheme $share/konsole
+cp color-schemes/MoeDark.colors $share/color-schemes
+
+cp konsole/MoeDark.colorscheme $share/konsole
+
+cp -r look-and-feel $share/plasma/look-and-feel
+
+cp -r theme $share/plasma/desktoptheme/
 
 mkdir -p $share/aurorae/themes
-cp -r MoeDark-aurorae/MoeDark aurorae/Moe $share/aurorae/themes
+cp -r aurorae/my-theme aurorae/Moe $share/aurorae/themes
