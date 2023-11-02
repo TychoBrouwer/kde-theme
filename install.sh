f362ee61f9c3f3cd259243480706e05ee9c3d9a4
Tycho Brouwer <tychobrouwer33@gmail.com>
2023-11-02 10:35:18 +0100

    fix paths

--- install.sh
+++ install.sh
@@ -11,4 +11,4 @@ cp -r look-and-feel $share/plasma/look-and-feel
 cp -r theme $share/plasma/desktoptheme/
 
 mkdir -p $share/aurorae/themes
-cp -r aurorae/my-theme aurorae/Moe $share/aurorae/themes
+cp -r aurorae/my-theme $share/aurorae/themes
