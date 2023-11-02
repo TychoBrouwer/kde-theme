02efd4eb694404e085319973ed9bb0df1bf4899e
Tycho Brouwer <tychobrouwer33@gmail.com>
2023-11-02 13:56:25 +0100

    wallpaper

--- install.sh
+++ install.sh
@@ -12,3 +12,9 @@ cp -r my-theme $share/plasma/desktoptheme/
 
 mkdir -p $share/aurorae/themes
 cp -r aurorae/my-theme $share/aurorae/themes
+
+mkdir -p $share/sddm/themes
+cp -r sddm/my-theme $share/sddm/themes
+
+mkdir -p $share/wallpapers
+cp -r wallpapers/my-theme $share/wallpapers
