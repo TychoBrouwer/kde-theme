fa6735b5d4141cd3cc38a0e33bff4b2855dcd119
Tycho Brouwer <tychobrouwer33@gmail.com>
2023-11-02 11:37:00 +0100

    simlink undo

--- install.sh
+++ install.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 share=/usr/share
-base=$HOME/Repositories/kde-theme
 
-sudo cp -sf $base/color-schemes/my-theme.colors $share/color-schemes
+cp color-schemes/my-theme.colors $share/color-schemes
 
-sudo cp -sf $base/konsole/my-theme.colorscheme $share/konsole
+cp konsole/my-theme.colorscheme $share/konsole
 
-sudo cp -sfr $base/look-and-feel/my-theme $share/plasma/look-and-feel/
+cp -r look-and-feel/my-theme $share/plasma/look-and-feel/
 
-sudo cp -sfr $base/my-theme $share/plasma/desktoptheme/
+cp -r my-theme $share/plasma/desktoptheme/
 
-sudo mkdir -p $share/aurorae/themes
-sudo cp -sfr $base/aurorae/my-theme $share/aurorae/themes
+mkdir -p $share/aurorae/themes
+cp -r aurorae/my-theme $share/aurorae/themes
 
