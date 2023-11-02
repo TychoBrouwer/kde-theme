ebacdda1ad752e9c31415770bf0c684784b00e39
Tycho Brouwer <tychobrouwer33@gmail.com>
2023-11-02 11:34:15 +0100

    test

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@
 share=/usr/share
 base=$HOME/Repositories/kde-theme
 
-cp -sf $base/color-schemes/my-theme.colors $share/color-schemes
+sudo cp -sf $base/color-schemes/my-theme.colors $share/color-schemes
 
-cp -sf $base/konsole/my-theme.colorscheme $share/konsole
+sudo cp -sf $base/konsole/my-theme.colorscheme $share/konsole
 
-cp -sf $base/look-and-feel/my-theme $share/plasma/look-and-feel/
+sudo cp -sf $base/look-and-feel/my-theme $share/plasma/look-and-feel/
 
-cp -sf $base/my-theme $share/plasma/desktoptheme/
+sudo cp -sf $base/my-theme $share/plasma/desktoptheme/
 
-mkdir -p $share/aurorae/themes
-cp -sf $base/aurorae/my-theme $share/aurorae/themes
+sudo mkdir -p $share/aurorae/themes
+sudo cp -sf $base/aurorae/my-theme $share/aurorae/themes
 
