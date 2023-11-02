eff99c2306c59769f1bb330889a016a421b3a5ff
Tycho Brouwer <tychobrouwer33@gmail.com>
2023-11-03 00:26:54 +0100

    test

--- install.sh
+++ install.sh
@@ -4,17 +4,9 @@ share=/usr/share
 
 cp color-schemes/my-theme.colors $share/color-schemes
 cp konsole/my-theme.colorscheme $share/konsole
-cp -r look-and-feel/my-theme $share/plasma/look-and-feel/
-cp -r my-theme $share/plasma/desktoptheme/
 
-mkdir -p $share/aurorae/themes
-cp -r aurorae/my-theme $share/aurorae/themes
-
-mkdir -p $share/sddm/themes
-cp -r sddm/my-theme $share/sddm/themes
-
-mkdir -p $share/wallpapers
-cp -r wallpapers/my-theme $share/wallpapers
-
-mkdir -p $share/Kvantum
-cp -r kvantum/my-theme $share/Kvantum
+cp -R plasma $share
+cp -R aurorae $share
+cp -R sddm $share
+cp -R wallpapers $share
+cp -R kvantum $share
