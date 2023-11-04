8293adf9f8324cfeea2aa500c56a16813340d0e9
Tycho Brouwer <tychobrouwer33@gmail.com>
2023-11-04 18:26:11 +0100

    fix script

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 share=/usr/share
 
-rm -Rf $share/plasma/desktoptheme/my-theme
-rm -Rf $share/plasma/look-and-feel/my-theme
-rm -Rf $share/aurorae/themes/my-theme
-rm -Rf $share/sddm/themes/my-theme
-rm -Rf $share/Kvantum/my-theme
-rm -Rf $share/wallpapers/my-theme
+[ -e "$share/plasma/desktoptheme/my-theme" ] && rm -R "$share/plasma/desktoptheme/my-theme"
+[ -e "$share/plasma/look-and-feel/my-theme" ] && rm -R "$share/plasma/look-and-feel/my-theme"
+[ -e "$share/aurorae/themes/my-theme" ] && rm -R "$share/aurorae/themes/my-theme"
+[ -e "$share/sddm/themes/my-theme" ] && rm -R "$share/sddm/themes/my-theme"
+[ -e "$share/Kvantum/my-theme" ] && rm -R "$share/Kvantum/my-theme"
+[ -e "$share/wallpapers/my-theme" ] && rm -R "$share/wallpapers/my-theme"
 
 cp -R color-schemes $share
 cp -R konsole $share
