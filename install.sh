bb33eef7b8c94a4e785a2c9378f841cda6b549ea
Tycho Brouwer <tychobrouwer33@gmail.com>
2023-11-27 12:47:25 +0100

    icon theme

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ share=/usr/share
 [ -e "$share/Kvantum/my-theme" ] && rm -R "$share/Kvantum/my-theme"
 [ -e "$share/wallpapers/my-theme" ] && rm -R "$share/wallpapers/my-theme"
 [ -e "$share/themes/my-theme" ] && rm -R "$share/themes/my-theme"
+[ -e "$share/icons/my-theme" ] && rm -R "$share/icons/my-theme"
 
 cp -R color-schemes $share
 cp -R konsole $share
@@ -18,3 +19,13 @@ cp -R sddm $share
 cp -R wallpapers $share
 cp -R Kvantum $share
 cp -R themes $share
+
+ln -s "$share/icons/my-theme/48x48" "$share/icons/my-theme/96x96"
+ln -s "$share/icons/my-theme/64x64" "$share/icons/my-theme/128x128"
+ln -s "$share/icons/my-theme/16x16" "$share/icons/my-theme/16x16@2x"
+ln -s "$share/icons/my-theme/16x16" "$share/icons/my-theme/18x18@2x"
+ln -s "$share/icons/my-theme/16x16" "$share/icons/my-theme/22x22@2x"
+ln -s "$share/icons/my-theme/16x16" "$share/icons/my-theme/24x24@2x"
+ln -s "$share/icons/my-theme/32x32" "$share/icons/my-theme/32x32@2x"
+ln -s "$share/icons/my-theme/48x48" "$share/icons/my-theme/48x48@2x"
+ln -s "$share/icons/my-theme/64x64" "$share/icons/my-theme/64x64@2x"
